4a8b551fc49c7dd6e30b49f8ac40e1cc11797656
Mayara-Souza <mayara.souza1523@gmail.com>
2024-02-08 20:33:31 -0300

    code: Separating Responsabilities

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,10 +1,18 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
+    '''
+    responsability for interacting with HTTP
+    '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
 
-        print(http_request)
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        body = http_request.body
+        product_code = body["product_code"]
+
+        # logica de regra de negocio
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
